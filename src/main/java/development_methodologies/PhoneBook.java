4467d2df540f4962fa71a647c4f1c31663394755
Irlond100 <irlondito100@gmail.com>
2023-07-01 21:02:27 +0300

    Stub and tests for add

--- src/main/java/development_methodologies/PhoneBook.java
+++ src/main/java/development_methodologies/PhoneBook.java
@@ -1,6 +1,45 @@
 package development_methodologies;
 
-public class PhoneBook {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class PhoneBook {
+	
+	private Map<String, List<String>> phoneBook;
+	
+	public PhoneBook() {
+		this.phoneBook = new HashMap<>();
+	}
+	
+	// добавляет контакты
+	public int addContact(String name, String phone) {
+		return 0;
+	}
+	
+	// находит имя по номеру без полного перебора
+	public List<String> getPhonesByName(String name) {
+		return phoneBook.get(name);
+	}
+	
+	// находит номер по имени без полного перебора.
+	public List<String> findByName(String name) {
+		return null;
+	}
+	
+	// выводит все имена в алфавитном порядке без необходимости проводить сортировку.
+	public void printAllNames() {
+	
+	}
+	
+	public boolean containsKey(String name) {
+		return phoneBook.containsKey(name);
+	}
+	
+	public List<String> get(String name) {
+		return phoneBook.get(name);
+	}
 	
 }
+
+
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,43 @@
-public class PhoneBookTest {
+import development_methodologies.PhoneBook;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PhoneBookTest {
+	
+	static PhoneBook phoneBook = new PhoneBook();
+	
+	@BeforeAll
+	public static void setUp() {
+		phoneBook.addContact("John", "1234567890");
+		phoneBook.addContact("John", "9876543210");
+		phoneBook.addContact("Alice", "5555555555");
+		phoneBook.addContact("Rlo", "987654");
+	}
+
+	@Test
+	public void testAddContact() {
+
+// Проверяем, что контакт был добавлен успешно
+		assertTrue(phoneBook.containsKey("John"));
+		List<String> phones = phoneBook.get("John");
+		assertTrue(phones.contains("1234567890"));
+
+// Проверяем, что в контакте "John" есть оба номера телефона
+		phones = phoneBook.get("John");
+		assertTrue(phones.contains("1234567890"));
+		assertTrue(phones.contains("9876543210"));
+
+// Проверяем, что контакт "Alice" был добавлен успешно
+		assertTrue(phoneBook.containsKey("Alice"));
+		phones = phoneBook.get("Alice");
+		assertTrue(phones.contains("5555555555"));
+	}
+	
 }
+
+
